bc2654af5d1adceea6ae169ba37b9765889a8b3f
Wenbin Wang <ecnuwbwang@gmail.com>
2017-12-07 03:37:26 -0800

    showing the moves

--- src/demo/game.js
+++ src/demo/game.js
@@ -33,6 +33,17 @@ class Game extends React.Component {
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -51,7 +62,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -72,6 +83,11 @@ class Game extends React.Component {
       xIsNext: !this.state.xIsNext,
     });
   }
+
+  jumpTo(step) {
+    console.log(`Jump to ${step}`)
+  }
+
 }
 
 export { Game };
