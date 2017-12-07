9bcbd0cd8134867066ec5c8a9d905c746411b991
Wenbin Wang <ecnuwbwang@gmail.com>
2017-12-07 03:11:27 -0800

    add declaring a winner

--- src/demo/board.js
+++ src/demo/board.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Square } from './square';
 
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b]
+      && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -19,6 +35,9 @@ class Board extends React.Component {
 
   handleClick(index) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
     squares[index] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -27,8 +46,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const nextPlayer = this.state.xIsNext ? 'X' : 'O';
-    const status = 'Next player: ' + nextPlayer;
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      const nextPlayer = this.state.xIsNext ? 'X' : 'O';
+      status = 'Next player: ' + nextPlayer;
+    }
     return (
       <div>
         <div className="status">{status}</div>
